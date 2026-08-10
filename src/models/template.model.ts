face238cd1abfc19307d68f6567207b9bc68b96c
xlaoru <sia212007@gmail.com>
2026-08-10 15:08:09 +0300

    Add requirements

--- src/models/template.model.ts
+++ src/models/template.model.ts
@@ -1,7 +1,10 @@
 import mongoose from 'mongoose'
 
 const templateSchema = new mongoose.Schema({
-  name: String,
+  name: {
+    type: String,
+    required: true,
+  },
   description: String,
   templateWorkout: [
     {
