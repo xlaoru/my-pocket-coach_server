32f814022a8a029ddbbdb0da3fae4b7d11d2188a
xlaoru <sia212007@gmail.com>
2026-08-16 22:15:16 +0300

    Fix misspelling

--- src/routes/exercise.route.ts
+++ src/routes/exercise.route.ts
@@ -1,24 +1,38 @@
-import { Router } from "express";
+import { Router } from 'express'
 
-import { createExercise, editExerciseName, addExerciseSet, editExerciseSet, removeExerciseSet, moveExercise, deleteExercise, } from "../controllers/exercise.contoller";
-import { isAuth } from "../middleware/is-auth.middleware";
+import {
+  addExerciseSet,
+  createExercise,
+  deleteExercise,
+  editExerciseName,
+  editExerciseSet,
+  moveExercise,
+  removeExerciseSet,
+} from '../controllers/exercise.controller'
+import { isAuth } from '../middleware/is-auth.middleware'
 
-const exerciseRouter = Router();
+const exerciseRouter = Router()
 
-exerciseRouter.post("/programs/:programId/exercises", isAuth, createExercise);
+exerciseRouter.post('/programs/:programId/exercises', isAuth, createExercise)
 
-exerciseRouter.patch("/programs/:programId/exercises/:exerciseId", isAuth, editExerciseName);
+exerciseRouter.patch('/programs/:programId/exercises/:exerciseId', isAuth, editExerciseName)
 
-exerciseRouter.post("/programs/:programId/exercises/:exerciseId/sets", isAuth, addExerciseSet);
+exerciseRouter.post('/programs/:programId/exercises/:exerciseId/sets', isAuth, addExerciseSet)
 
-exerciseRouter.patch("/programs/:programId/exercises/:exerciseId/sets/:setIndex", isAuth, editExerciseSet);
+exerciseRouter.patch(
+  '/programs/:programId/exercises/:exerciseId/sets/:setIndex',
+  isAuth,
+  editExerciseSet,
+)
 
-exerciseRouter.delete("/programs/:programId/exercises/:exerciseId/sets/:setIndex", isAuth, removeExerciseSet);
+exerciseRouter.delete(
+  '/programs/:programId/exercises/:exerciseId/sets/:setIndex',
+  isAuth,
+  removeExerciseSet,
+)
 
-exerciseRouter.patch("/programs/:programId/workout/move", isAuth, moveExercise)
+exerciseRouter.patch('/programs/:programId/workout/move', isAuth, moveExercise)
 
-exerciseRouter.delete("/programs/:programId/exercises/:exerciseId", isAuth, deleteExercise);
+exerciseRouter.delete('/programs/:programId/exercises/:exerciseId', isAuth, deleteExercise)
 
-export {
-    exerciseRouter
-}
+export { exerciseRouter }
